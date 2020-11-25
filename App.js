47147baa1027cd8eda8c42dc5682b283f583ee0e
DiemBTran <diembtran@gmail.com>
2020-11-25 15:17:37 -0800

    Creates a React Native app with an infinite-loading photo-stream
    Uses React hooks and React Native components

--- App.js
+++ App.js
@@ -1,14 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useReducer, useCallback } from 'react'
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
+
+import { getList } from './api/picsum'
+import { actionCreators, initialState, reducer } from './reducers/photos'
+import PhotoGrid from './components/PhotoGrid'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const { photos, nextPage, loading, error } = state
+
+  const fetchPhotos = useCallback(async () => {
+    dispatch(actionCreators.loading())
+
+    try {
+      const nextPhotos = await getList(nextPage)
+      dispatch(actionCreators.success(nextPhotos, nextPage))
+    } catch (e) {
+      dispatch(actionCreators.failure())
+    }
+  }, [nextPage])
+
+  useEffect(() => {
+    fetchPhotos()
+  }, [])
+
+  // We'll show an error only if the first page fails to load
+  if (photos.length === 0) {
+    if (loading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator animating={true} />
+        </View>
+      )
+    }
+
+    if (error) {
+      return (
+        <View style={styles.container}>
+          <Text>Failed to load photos!</Text>
+        </View>
+      )
+    }
+  }
+
+  return <PhotoGrid numColumns={3} photos={photos} onEndReached={fetchPhotos}/>
 }
 
 const styles = StyleSheet.create({
---
+++ reducers/photos.js
@@ -0,0 +1,38 @@
+const types = {
+    LOADING: 'LOADING',
+    SUCCESS: 'SUCCESS',
+    FAILURE: 'FAILURE',
+  }
+  
+  export const actionCreators = {
+    loading: () => ({ type: types.LOADING }),
+    failure: () => ({ type: types.FAILURE }),
+    success: (photos, page) => ({
+      type: types.SUCCESS,
+      payload: { photos, page },
+    }),
+  }
+
+  export const initialState = {
+    loading: false,
+    error: false,
+    photos: [],
+    nextPage: 1,
+  }
+
+  export function reducer(state, action) {
+    switch (action.type) {
+      case types.LOADING:
+        return { ...state, loading: true, error: false }
+      case types.SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          error: false,
+          photos: [...state.photos, ...action.payload.photos],
+          nextPage: state.nextPage + 1,
+        }
+      case types.FAILURE:
+        return { ...state, loading: false, error: true }
+    }
+  }
